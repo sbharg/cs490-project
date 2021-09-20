7c8c728df9431cf936fc8db990b64973e7e9a2e3
bhargavsamineni1 <bsamineni8@gmail.com>
2021-09-20 00:52:55 -0400

    more fixes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from models import User
 import argparse
 import helpers as help
 
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
 
 # For use on Heroku
 class ProductionConfig(Config):
-    SECRET_KEY = '\xbbriA9(\xee/y\x1e\xc6\xb6\xcb\xff\x81\xda]\x19\x05\xe4\x9ez\x19\xa3' 
+    SECRET_KEY = b'\xbbriA9(\xee/y\x1e\xc6\xb6\xcb\xff\x81\xda]\x19\x05\xe4\x9ez\x19\xa3' 
     SQLALCHEMY_DATABASE_URI = 'postgresql://uvyygsmbjrjsda:70dde68a91f363b964b2a581846c07d80284de8875af0fcb0dd7f6a053ca6d5f@ec2-54-146-84-101.compute-1.amazonaws.com:5432/dc9uf2dvu4i1t9'
 
 # For use when developing on local machines
