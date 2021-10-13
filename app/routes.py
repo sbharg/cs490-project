5b3765ee7f291825c2e406f7ddc46ef99861f0c9
ezhang123 <76235043+ezhang123@users.noreply.github.com>
2021-10-12 23:08:28 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import (
+    Blueprint, 
+    render_template, 
+    request,
+    url_for, 
+    redirect, 
+    g, 
+    session
+)
 from app import db
 import app.helpers as help
 from werkzeug.security import check_password_hash
@@ -11,17 +19,32 @@ login_bp = Blueprint(
     static_url_path='/static'
 )
 
+@login_bp.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        g.user = help.find_user_id(db, session['user_id'])
+
 @login_bp.route('/adminlanding')
 def adminlanding():
+    if not g.user or g.user.user_type != 'teacher':
+        return redirect(url_for('login_bp.index'))
     return render_template('adminlanding.html')
 
 @login_bp.route('/userlanding')
 def userlanding():
+    if not g.user or g.user.user_type != 'student':
+        return redirect(url_for('login_bp.index'))
     return render_template('userlanding.html')
 
 @login_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+##TODO add the sign in page support
+
 
 @login_bp.route('/login', methods = ['POST'])
 def login():
@@ -29,15 +52,19 @@ def login():
     Post method to handle login requests    
     '''
     if request.method == 'POST':
+        session.pop("user_id", None)
+
         username = request.form['username']
         password = request.form['password']
-        user = help.find_user(db, username)
+        user = help.find_username(db, username)
+
         if user is None or not check_password_hash(user.password_hash, password):
             return render_template('index.html', msg = "The username or password you entered does not match or is incorrect")
         elif check_password_hash(user.password_hash, password):
-            if user.user_type == "admin":
+            session['user_id'] = user.id
+            if user.user_type == "teacher":
                 return redirect(url_for('login_bp.adminlanding'))
-            elif user.user_type == "user":
+            elif user.user_type == "student":
                 return redirect(url_for('login_bp.userlanding'))
         else:
             return "Something went wrong"
