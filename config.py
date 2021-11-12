24a18f242c167b8b4e97827ed3a71916f1cfe78e
bhargavsamineni1 <bsamineni8@gmail.com>
2021-11-11 20:07:16 -0500

    updated config to make debugging easier

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ class ProductionConfig(Config):
 # For use when developing on local machines
 class DevelopmentConfig(Config):
     DEBUG = True
+    SECRET_KEY = b'\xbbriA9(\xee/y\x1e\xc6\xb6\xcb\xff\x81\xda]\x19\x05\xe4\x9ez\x19\xa3' 
     # Change to local database
-    SQLALCHEMY_DATABASE_URI = 'postgresql://bsamineni:@localhost/bsamineni'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://uvyygsmbjrjsda:70dde68a91f363b964b2a581846c07d80284de8875af0fcb0dd7f6a053ca6d5f@ec2-54-146-84-101.compute-1.amazonaws.com:5432/dc9uf2dvu4i1t9'
